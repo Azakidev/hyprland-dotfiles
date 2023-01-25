1fa28c5d20d36027bc4a875df28138e293d1f078
Azakidev <zazaguichi@outlook.com>
2023-01-25 02:14:05 +0100

    Tweaks

--- fuzzel/fuzzel-search.sh
+++ fuzzel/fuzzel-search.sh
@@ -5,7 +5,7 @@
 search=$(fuzzel --dmenu --prompt="Search: " --width=30 --lines=0)
 if [ -z "$search" ]; then exit; fi
 
-result=$(ddgr --json $search | tee /tmp/search.tmp | jq -r -S '"\(.[].title) - \(.[].url) -", .[].abstract' | fuzzel --dmenu | awk '{1 ; sub(/ - http.*/, ""); print}')
+result=$(ddgr --json $search | tee /tmp/search.tmp | jq -r -S '"\(.[].title) - \(.[].url)", .[].abstract' | fuzzel --dmenu | awk '{1 ; sub(/ - http.*/, ""); print}')
 if [ -z "$result" ]; then exit; fi
 
 url=$(cat /tmp/search.tmp | jq -r ".[] | select(.title == \"$result\") | .url")
